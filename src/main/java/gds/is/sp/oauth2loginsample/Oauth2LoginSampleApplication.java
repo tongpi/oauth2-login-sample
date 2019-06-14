c6ad6a3cafed523e62e983226c8fff2300e65d1d
wangf <ten2net@gmail.com>
2019-06-14 12:06:44 +0800

    切换到https访问IS。要么在本地导入IS服务器证书公钥，要么如本例这样禁用hostnameVerify

--- src/main/java/gds/is/sp/oauth2loginsample/Oauth2LoginSampleApplication.java
+++ src/main/java/gds/is/sp/oauth2loginsample/Oauth2LoginSampleApplication.java
@@ -1,13 +1,61 @@
 package gds.is.sp.oauth2loginsample;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.servlet.ServletException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Oauth2LoginSampleApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ServletException {
+		disableHostNameVerify();		
 		SpringApplication.run(Oauth2LoginSampleApplication.class, args);
+	}
+
+	private static void disableHostNameVerify() throws ServletException {
+		try {
+
+			SSLContext sc;
+
+			// Get SSL context
+			sc = SSLContext.getInstance("SSL");
+
+			// Create empty HostnameVerifier
+			HostnameVerifier hv = new HostnameVerifier() {
+				public boolean verify(String urlHostName, SSLSession session) {
+					return true;
+				}
+			};
+
+			// Create a trust manager that does not validate certificate chains
+			TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+				public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+					return null;
+				}
+
+				public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+				}
+
+				public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+				}
+			} };
+
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+			// SSLSocketFactory sslSocketFactory = sc.getSocketFactory();
+
+			// HttpsURLConnection.setDefaultSSLSocketFactory(sslSocketFactory);
+			SSLContext.setDefault(sc);
+			HttpsURLConnection.setDefaultHostnameVerifier(hv);
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
 	}
 
 }
